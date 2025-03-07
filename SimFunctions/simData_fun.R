36f0487e2a710850124859f7fe8cff2359bd201f
lcpowers <lipo6619@colorado.edu>
2025-03-07 12:37:01 -0700

    Big changes to function that simulates population data. Vectorized for loops. Moved other files.

--- SimFunctions/simData_fun.R
+++ SimFunctions/simData_fun.R
@@ -3,284 +3,191 @@ simulate_pop_data <- function(surv_params, growth_params, seeds,
   
   # Set the microhabitat effect parameters
   growth_params$asp.mag <- surv_params$asp.mag <- asp.mag # Shift distance
-
-  # New pop df for each subpop
   n.subpops <- length(asp.effects)
+  
+  # New pop df for each subpop
+  seed_counts <- rep(0, n.subpops) # ADDED
   subpop.list.t0 <- list()
   
   for(n in 1:n.subpops){
     
     growth_params$asp.effect <- surv_params$asp.effect <- asp.effects[n]
-
+    
     ## Finding the Subpop specific SSD for when asp.effects asymmetrical
     mean.mx <- subpop_mx_fun(surv_params = surv_params, growth_params = growth_params, seeds.in = seeds)
+    ssd.props <- Re(eigen(mean.mx)$vectors[,1])/Re(sum(eigen(mean.mx)$vectors[,1]))
+    start_pop <- round(ssd.props * (50/ssd.props[3]) )
 
-    ssd.props <- eigen(mean.mx)$vectors[,1]/sum(eigen(mean.mx)$vectors[,1])
-    ssd.scalar <- 50/ssd.props[3]
-    start_pop <- round(ssd.props*ssd.scalar)
-    # print(start_pop)
-    total_pop <- sum(start_pop)
-    
-    # Initialize population 
-    pop <- data.frame(
-      individual_id = 1:total_pop,
+    juv_flrg_pop <- sum(start_pop[2:3])
+    dt.n <- data.table(
+      individual_id = seq_len(juv_flrg_pop),
       start.stage = NA,
-      end.stage = c(rep("seed",start_pop[1]),rep("juv",start_pop[2]),rep("flrg",start_pop[3])),
-      survival_status = rep(1, total_pop),  # Track if they survive (1 = survive, 0 = don't survive)
-      growth_status = rep(NA, total_pop),   # Track growth status (NA = not alive, 1 = grew, 0 = didn't grow)
-      repro_status = rep(NA, total_pop),    # Track reproduction events to make sure the align with probability
-      timestep = rep(0, total_pop),         # Start at timestep 0
-      stringsAsFactors = FALSE
-    ) %>% 
-      mutate(seeds = NA,
-             clim = NA,
-             asp.mag = asp.mag,
-             asp.effect = asp.effects[n])
-    
-    assign(paste0("subpop.",n),pop)
-    subpop.list.t0[[n]] <- eval(as.name(paste0("subpop.",n)))
-    rm(pop)
+      end.stage = rep(c("juv", "flrg"), start_pop[2:3]),
+      survival_status = 1,
+      growth_status = 0,
+      repro_status = 0,
+      timestep = 0,
+      seeds = 0,
+      clim = -9999,
+      asp.mag = asp.mag,
+      asp.effect = asp.effects[n]
+    ) 
+
+    dt.n$seeds[dt.n$end.stage=="flrg"][1] <- start_pop[1]
+    subpop.list.t0[[n]] <- dt.n
+    rm(dt.n)
+    seed_counts[n] <- start_pop[1]
     
   }
-
-  all_subpops <- data.table::rbindlist(subpop.list.t0)
-  # Store population data for each timestep in a list
+  
   all_population_data <- list()
-  all_population_data[[1]] <- all_subpops;rm(all_subpops)  # Store initial state
+  all_population_data[[1]] <- data.table::rbindlist(subpop.list.t0)
 
   # Start to loop through timesteps
   for (t in 1:timesteps) {
-
-    # print(t)
-    # t = 2
+    # t = 1
+    ## initiate subpop list
     subpop.list.t1 <- list()
-    new_seeds_vec <- rep(0,n.subpops)
+    new_seed_counts <- rep(0,n.subpops)
 
+    # Put clim value into the survival and growth parameter lists
+    clim.t <- clim_vals[t]
+    growth_params$clim <- surv_params$clim  <- clim.t
+    
     for(n in 1:n.subpops){
-
-      # Get subpop.n dataframe
-      subpop.n <- subpop.list.t0[[n]] %>%
-        mutate(start.stage = end.stage,
-               end.stage = NA)
-
-      # *** CHECK FOR ZERO SUBPOPULATION HERE ***
-      if (is.null(subpop.n)) {
-
-        # If the output from the. list is NULL go to next loo
-        next  # Uncomment to stop the simulation entirely
-
-        } else {
-
-          # Otherwise, filter for survived individuals
-        subpop.n <- filter(subpop.n, survival_status == 1)
-
-      }
-
-      # And if noone survived last year
-      if (nrow(subpop.n)==0) {
-
-        next  # Carry on to next loop
-
-        }
-
-      # Drop rows of individuals that died in the last time step
-      # subpop.n <- filter(subpop.n,survival_status==1)
-
+      
       # Add aspect effect to growth and surv params
       growth_params$asp.effect <- surv_params$asp.effect <- asp.effects[n] # Shift direction
+      
+      # Get last timestep dt for subpop n
+      subpop.n <- subpop.list.t0[[n]]
+      
+      # Filter for living individuals
+      subpop.n <- subpop.n[survival_status == 1]
+      if (nrow(subpop.n) == 0) subpop.n <- data.table()
+      
+      # Process prior year's seeds into juveniles
+      seed_count <- seed_counts[n] # ADDED
+      if (seed_count > 0) {
 
-      # Get clim value from input vector of clim values. Input vector rather than selected here to make sure that the same values are used for different microhabitats/subpops
-      clim.t <- clim_vals[t]
+        # Seed survival and growth rates 
+        P_surv <- do.call(survival_fun, surv_params)["seed"]
+        P_grow <- do.call(growth_fun, growth_params)["seed"]
+        
+        # Number of new juveniles this year from seeds last year
+        n_new_juvs <- rbinom(1, seed_count, P_surv*P_grow)
+        
+        # If there are new juveniles, add them to the data.table
+        if (n_new_juvs > 0) {
+          new_juvs_dt <- data.table(
+            individual_id = if (nrow(subpop.n) > 0) max(subpop.n$individual_id, na.rm = TRUE) + 1:n_new_juvs else 1:n_new_juvs,
+            start.stage = "seed",
+            end.stage = "juv",
+            survival_status = 1,
+            growth_status = 1,
+            repro_status = 0,
+            timestep = t,
+            seeds = 0,
+            clim = clim.t,
+            asp.mag = asp.mag,
+            asp.effect = asp.effects[n]
+          )
+          # subpop.n <- rbind(subpop.n, new_juvs_dt)
+        } # End adding new juveniles
+        
+      } # End processing last years seeds
 
-      # Put clim value into the survival and growth parameter lists
-      growth_params$clim <- surv_params$clim  <- clim.t
-      subpop.n$clim <- clim.t # Store this timestep's climate value
-
-      # Update survival and growth rates based on the new clim
-      surv.rates <- do.call(survival_fun, surv_params)
-      growth.rates <- do.call(growth_fun, growth_params)
-
-      # Process each individual in the subpopulation
-      for (i in 1:nrow(subpop.n)) {
-
-        # Update timestep for each individual
-        subpop.n$timestep[i] <- t
-
-        if (subpop.n$start.stage[i] == "seed") {
-
-          # This is a very stepwise way to do this...
-
-          # Survival and growth probabilities for seeds
-          survival_prob <- surv.rates["seed"]
-          growth_prob <- growth.rates["seed"]
-
-          # print(paste("Timestep:", t, "Subpopulation:", n, "Row:", i))
-          # print(paste("survival_prob:", survival_prob))
-
-          # Survival status
-          subpop.n$survival_status[i] <- rbinom(1, 1, survival_prob)
-
-          # If survived, grow?
-          if (subpop.n$survival_status[i] == 1) {
-
-            # Growth (transition to juvenile) status
-            subpop.n$growth_status[i] <- rbinom(1, 1, growth_prob)
-
-            # If growth occurs, move to juvenile stage
-            if (subpop.n$growth_status[i] == 1) {
-
-              subpop.n$end.stage[i] <- "juv"
-              # seed_to_juv = seed_to_juv + 1
-
-            } else if(subpop.n$growth_status[i] == 0){
-
-              # This else if wasn't here before, but without it, seeds can survive without transitioning to juv stage.
-              # i.e. there was a seed bank
-              subpop.n$end.stage[i] <- NA
-              subpop.n$survival_status[i] <- 0
-
-            }
-
-          }
-
-        } else if (subpop.n$start.stage[i] == "juv") {
-
-          # Survival and growth probabilities for juveniles
-          survival_prob <- surv.rates["juv"]
-          growth_prob <- growth.rates["juv"]
-
-          # Determine survival
-          subpop.n$survival_status[i] <- rbinom(1, 1, survival_prob)
-
-          # If survived, grow
-          if (subpop.n$survival_status[i] == 1) {
-            subpop.n$growth_status[i] <- rbinom(1, 1, growth_prob)
-
-            # If growth occurs, move to repro/flowering stage
-            if (subpop.n$growth_status[i] == 1) {
-              subpop.n$end.stage[i] <- "flrg"
-            } else {
-
-              subpop.n$end.stage[i] <- "juv"
-
-            }
-          }
-
-        } else if (subpop.n$start.stage[i] == "flrg") { ## Flowering stage
-          # Survival probs flowering plants (no growth possible)
-          survival_prob <- surv.rates["flrg"]
-
-          # Determine survival
-          subpop.n$survival_status[i] <- rbinom(1, 1, survival_prob)
-
-          # If survived, reproduce and produce new seeds
-          if (subpop.n$survival_status[i] == 1) {
-
-            subpop.n$end.stage[i] <- "flrg"
-
-            # Prob reproduction
-            subpop.n$repro_status[i] <- rbinom(1, 1, P.flr)
-
-            # If the reproductive-age flower did in fact produce flowers, how many seeds?
-            if(subpop.n$repro_status[i] == 1){
-
-              new_seeds <- seeds #rpois(n = 1,lambda = seed_vec)
-              subpop.n$seeds[i] <- new_seeds # Store seed number to check later
-              # total_new_seeds <- total_new_seeds + new_seeds
-
-            } # End seeds
-
-          } # End repro
-
-        } # End flrg stage
-
-      } # End loop going through each row of subpop dataframe
-
-      subpop.n$end.stage[subpop.n$survival_status==0] = "dead"
-
-      new_seeds_vec[n] = sum(subpop.n$seeds,na.rm=T)
-      assign(paste0("subpop.",n),subpop.n)
-      rm(subpop.n)
-
+      # If there are still living individuals to track
+      if (nrow(subpop.n) > 0) {
+        
+        # Set last year's end.stage to this years start.stage
+        subpop.n[, `:=` (start.stage = end.stage, end.stage = NA, timestep = t,
+                         survival_status=0, growth_status=0, repro_status=0,
+                         seeds = 0, clim = clim.t)]
+        
+        # Update survival and growth rates based on the new clim
+        surv.rates <- do.call(survival_fun, surv_params)
+        growth.rates <- do.call(growth_fun, growth_params)
+        
+        # Used if tracking every individual seed
+        # subpop.n[start.stage == "seed", survival_status := rbinom(.N, 1, surv.rates["seed"])]
+        # subpop.n[start.stage == "seed" & survival_status == 1, growth_status := rbinom(.N, 1, growth.rates["seed"])]
+        # subpop.n[start.stage == "seed" & growth_status == 1, end.stage := "juv"]
+        
+        subpop.n[start.stage == "juv", survival_status := rbinom(.N, 1, surv.rates["juv"])]
+        subpop.n[start.stage == "juv" & survival_status == 1, growth_status := rbinom(.N, 1, growth.rates["juv"])]
+        subpop.n[start.stage == "juv" & growth_status == 1, end.stage := "flrg"]
+        subpop.n[start.stage == "juv" & survival_status == 1 & growth_status == 0, end.stage := "juv"]
+        subpop.n[start.stage == "juv" & survival_status == 0, end.stage := "dead"]
+        
+        subpop.n[start.stage == "flrg", survival_status := rbinom(.N, 1, surv.rates["flrg"])]
+        subpop.n[start.stage == "flrg" & survival_status == 1, repro_status := rbinom(.N, 1, P.flr)]
+        subpop.n$seeds[subpop.n$start.stage == "flrg" & subpop.n$repro_status == 1] = seeds 
+        subpop.n[start.stage == "flrg" & survival_status == 1, end.stage := "flrg"]
+        subpop.n[start.stage == "flrg" & survival_status == 0, end.stage := "dead"]
+        
+        # Fill in empty end.stage cells
+        # subpop.n[is.na(end.stage) & survival_status == 1 & growth_status == 0, end.stage := start.stage]
+        # subpop.n[survival_status == 0, end.stage := "dead"]
+        
+        new_seed_counts[n] <- sum(subpop.n$seeds)
+         
+      }
+      
+    if( exists("new_juvs_dt") ) {subpop.n <- rbind(subpop.n,new_juvs_dt)}
+      
+    subpop.list.t1[[n]] <- subpop.n
+      
     } # End subpopulation fates dataframe
-
-    ### Dealing with new seeds and seed movement
-
+    
+    ### Dealing with seed movement
+    
     # Add new seeds to the population dataframe
-    if (sum(new_seeds_vec) > 0) {
-
+    if (sum(new_seed_counts) > 0) {
+      
       # movement matrix to adjust seed vector
       move.mx <- matrix(data = NA,
                         nrow = n.subpops,
                         ncol = n.subpops)
       diag(move.mx) <- 1 - move.rate # Along diagonal is what stays
       move.mx[is.na(move.mx)] <- move.rate/(n.subpops-1) # Other elements are what moves
-      new_seeds_move_vec <- c(move.mx%*%new_seeds_vec)
-
-      for(n in 1:n.subpops){
-
-        new_seeds <- round(sum(new_seeds_move_vec[n]))
-
-        # Individual IDs for new seeds
-        subpop.n <- eval(as.name(paste0("subpop.",n)))
-        new_individual_ids <-(max(subpop.n$individual_id)+1):(max(subpop.n$individual_id)+new_seeds)
-
-        new_seeds_df <- data.frame(
-          individual_id = new_individual_ids,
-          start.stage = NA,
-          end.stage = rep("seed", new_seeds),
-          survival_status = rep(1, new_seeds),
-          growth_status = rep(NA, new_seeds),
-          repro_status = rep(NA, new_seeds),
-          timestep = rep(t, new_seeds),
-          clim = rep(clim.t,new_seeds),
-          stringsAsFactors = FALSE
-        ) %>%
-          mutate(seeds = NA,
-                 asp.mag = asp.mag,
-                 asp.effect = asp.effects[n])
-
-        subpop.n <- rbind(subpop.n,new_seeds_df)
-        subpop.list.t1[[n]] <- subpop.n
-      }
-
-    } # End adding and moving new seeds
-
-    # Turn subpop list of dataframes into one dataframe
-    all_subpops <- data.table::rbindlist(subpop.list.t1)
-
-    # add that to the all population list
-    all_population_data[[t+1]] <- all_subpops
-
+      new_seed_counts_move <- as.numeric(move.mx%*%new_seed_counts)
+      new_seed_counts <- new_seed_counts_move;rm(new_seed_counts)
+    }
+    
+    # Turn subpop list in dataframe and add to output list
+    all_subpops <- rbindlist(subpop.list.t1, use.names = TRUE, fill = F)
+    all_population_data[[t + 1]] <- all_subpops
+    
     # Set this years subpops equal to t0 and remove t1
     subpop.list.t0 <- subpop.list.t1;rm(subpop.list.t1)
-
+    
     # *** CHECK FOR ZERO TOTAL POPULATION HERE ***
-    if (nrow(all_subpops)==0) {
-
-      # If theres no one left, break the loop/stop the sim
-      break
-    }
-
+    # If theres no one left, break the loop/stop the sim
+    if (nrow(all_subpops) == 0 && sum(seed_counts) == 0) break
     rm(all_subpops)
-
+    
   } # End timestep loop
-
-  all_population_output <- data.table::rbindlist(all_population_data, use.names = TRUE, fill = TRUE)
-
-  if(max(all_population_output$timestep)<timesteps){
-    extinct_df <- expand.grid(individual_id = -9999,
-                              start.stage = "extinct",
-                              end.stage = NA,
-                              survival_status=0,
-                              growth_status=0,
-                              repro_status=0,
-                              timestep = seq((max(all_population_output$timestep)+1),timesteps,1),
-                              seeds = NA,
-                              clim = NA,
-                              asp.mag = NA,
-                              asp.effect = asp.effects)
-    all_population_output <- rbind(all_population_output,extinct_df)
-  }
+  
+  all_population_output <- rbindlist(all_population_data, use.names = TRUE, fill = TRUE)
+  
+  if (max(all_population_output$timestep) < timesteps) {
+    extinct_df <- data.table(
+      individual_id = -9999,
+      start.stage = "extinct",
+      end.stage = NA,
+      survival_status = 0,
+      growth_status = 0,
+      repro_status = 0,
+      timestep = seq(max(all_population_output$timestep) + 1, timesteps),
+      seeds = 0,
+      clim = NA,
+      asp.mag = asp.mag,
+      asp.effect = asp.effects
+    )
+    
+    all_population_output <- rbind(all_population_output, extinct_df, fill = TRUE)
+  
+    }
   return(all_population_output)
 } # End function
